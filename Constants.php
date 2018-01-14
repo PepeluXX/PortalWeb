3ff2b8fabac7c917612dce119285a49f178d41ab
PepeluXX <32452569+PepeluXX@users.noreply.github.com>
2018-01-14 02:14:11 +0100

    Update Constants.php

--- Constants.php
+++ Constants.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_NAME','version5');
-define('DB_USER','root');
-define('DB_PASSWORD','CG9ox92WAzKIptWj');
-define('DB_HOST','localhost');
+define('DB_NAME','*');
+define('DB_USER','*');
+define('DB_PASSWORD','*');
+define('DB_HOST','*');
